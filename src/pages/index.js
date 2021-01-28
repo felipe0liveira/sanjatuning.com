36543dad02c65c5236dfc292702ad1af6bb8abb0
felipe0liveira <felpd3v@gmail.com>
2021-01-28 17:53:58 -0300

    feat(): Creates the Index Page

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import Avatar from '../components/avatar'
+import Panel from '../components/panel'
+import './index.scss'
+
+const Index = () => {
+  return (
+    <>
+      <Panel>
+        <Avatar />
+      </Panel>
+    </>
+  )
+}
+
+export default Index
