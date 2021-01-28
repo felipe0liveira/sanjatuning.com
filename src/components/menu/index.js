0b6426cb3ef1642ecdd57bcac7db3c5f99e6d6bf
felipe0liveira <felpd3v@gmail.com>
2021-01-28 18:18:25 -0300

    feat(): Updates the menu component

--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -1,7 +1,19 @@
+import './index.scss'
+
 const Menu = () => {
   return (
     <>
-      <div className='menu'></div>
+      <div className='menu'>
+        <div className='item'>
+          <span>Youtube</span>
+        </div>
+        <div className='item'>
+          <span>Instagram</span>
+        </div>
+        <div className='item'>
+          <span>Seila</span>
+        </div>
+      </div>
     </>
   )
 }
