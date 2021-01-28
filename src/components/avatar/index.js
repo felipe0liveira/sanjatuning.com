085bdd343ca33b5f5e92ff7db55424ed3b0d76ab
felipe0liveira <felpd3v@gmail.com>
2021-01-28 17:53:12 -0300

    feat(): Creates the avatar component

---
+++ src/components/avatar/index.js
@@ -0,0 +1,14 @@
+import './index.scss'
+
+const Avatar = () => {
+  return (
+    <>
+      <div className='avatar'>
+        <img src='/images/avatar.png' alt='Avatar SanjaTuning' />
+        <h1>SanjaTuning</h1>
+      </div>
+    </>
+  )
+}
+
+export default Avatar
